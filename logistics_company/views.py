cbaf69eeaf208073a8c706d6a56721ec041ffb49
DenisKhudyakov <denis.hudyakov27@gmail.com>
2024-06-26 22:01:20 +0500

    created filter and sorted

--- logistics_company/views.py
+++ logistics_company/views.py
@@ -1,6 +1,13 @@
 from django.shortcuts import render
 from django.views.generic import ListView
 
+from logistics_company.models import Registry
+
 
 class MainTableListView(ListView):
-    pass
+    template_name = 'logistics_company/main.html'
+    model = Registry
+
+    def get_queryset(self):
+        return Registry.objects.all()
+
