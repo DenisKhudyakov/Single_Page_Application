b52a1bfed85256fb489db6cb449cd053c28ff556
DenisKhudyakov <denis.hudyakov27@gmail.com>
2024-06-26 22:37:46 +0500

    create test

--- logistics_company/tests.py
+++ logistics_company/tests.py
@@ -1,3 +1,20 @@
 from django.test import TestCase
 
-# Create your tests here.
+from logistics_company.models import Registry
+
+
+class RegistryTestCase(TestCase):
+
+    def setUp(self):
+        Registry.objects.create(
+            date="25.11.2024",
+            name="Logistics Company",
+            count=5,
+            distance=500,
+
+        )
+
+    def test_registry_creation(self):
+        registry = Registry.objects.get(name="Logistics Company")
+        self.assertEqual(registry.name, "Logistics Company")
+        self.assertEqual(registry.distance, 500)
